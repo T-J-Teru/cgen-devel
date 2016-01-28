ea446b8fbeac6bd2044521c74338826150125390
Andrew Burgess <andrew.burgess@embecosm.com>
2016-01-28 11:22:28 +0000

    Update format and dates of generated copyright header
    The preferred format for the copyright header has changed slightly, and
    the dates we generate have lagged behind the actual date.  This commit
    updates the format, and the date so that when the opcodes directory of
    binutils-gdb is regenerated all of the copyright headers match those
    that are actually in the files.
    
    ChangeLog:
    
    	* utils.scm (copyright-fsf): Update format and dates in generated
    	copyright header.

--- utils.scm
+++ utils.scm
@@ -1310,7 +1310,7 @@
   (cons "\
 THIS FILE IS MACHINE GENERATED WITH CGEN.
 
-Copyright 1996-2010 Free Software Foundation, Inc.
+Copyright (C) 1996-2016 Free Software Foundation, Inc.
 "
 	"\
    This file is free software; you can redistribute it and/or modify
